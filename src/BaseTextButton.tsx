225dc80bbbbe3bf5b4f7cde83a4cfee682a7fc93
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-08-17 15:43:28 +0100

    feat: add baseTextButtonComponent

---
+++ src/BaseTextButton.tsx
@@ -0,0 +1,35 @@
+import React, { MouseEvent, ReactElement } from "react";
+import { makeStyles, ThemeProvider, Button } from "@material-ui/core";
+import { theme } from "./theme";
+
+interface Props {
+  text: string;
+  onClick?: (event: MouseEvent) => void;
+}
+
+export const BaseTextButton = (props: Props): ReactElement => {
+  const classes = makeStyles({
+    makeButton: {
+      "&:hover": {
+        backgroundColor: theme.palette.info.main,
+      },
+    },
+  })(props);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Button
+        className={classes.makeButton}
+        variant="contained"
+        color="primary"
+        onClick={props.onClick}
+      >
+        {props.text}
+      </Button>
+    </ThemeProvider>
+  );
+};
+
+BaseTextButton.defaultProps = {
+  onClick: null,
+};
