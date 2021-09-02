bc10044677967a5d228d620d7601375c50221c6a
Craig Cooper <c@c9r.dev>
2021-09-02 07:31:14 +0100

    fix: move imgSrc function out

--- src/index.tsx
+++ src/index.tsx
@@ -22,3 +22,5 @@ export { BaseIconButton } from "./BaseIconButton";
 export { BaseTextButton } from "./BaseTextButton";
 export { BaseTooltipTitle } from "./BaseTooltipTitle";
 export type { Tooltip, Tooltips } from "./tooltips";
+
+export const imgSrc = (src: string, type = "svg"): string => new URL(`/src/assets/${src}.${type}`, import.meta.url).href;
