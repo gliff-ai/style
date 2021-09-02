6f11c744d1db274552b2ef2bd66fa0c4dd61b3bb
Craig Cooper <c@c9r.dev>
2021-09-02 11:43:47 +0000

    fix: change imgSrc function

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ export type { Tooltip, Tooltips } from "./tooltips";
 
 export const imgSrc = (src: string, type = "svg"): string =>
   // Vite compiles this weirdly if it's  a template
-  // eslint-disable-next-line prefer-template 
+  // eslint-disable-next-line prefer-template
   new URL("/src/assets/" + src + "." + type, import.meta.url).href;
 
 export const generateClassName =
