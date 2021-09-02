780731092738895143d10b1b394f3a4d9c3fcd54
Craig Cooper <c@c9r.dev>
2021-09-02 12:29:25 +0000

    fix: remove imgSrc

--- src/index.tsx
+++ src/index.tsx
@@ -23,11 +23,6 @@ export { BaseTextButton } from "./BaseTextButton";
 export { BaseTooltipTitle } from "./BaseTooltipTitle";
 export type { Tooltip, Tooltips } from "./tooltips";
 
-export const imgSrc = (src: string, type = "svg"): string =>
-  // Vite compiles this weirdly if it's  a template
-  // eslint-disable-next-line prefer-template
-  new URL("/src/assets/" + src + "." + type, import.meta.url).href;
-
 export const generateClassName =
   (prefix: string) =>
   (rule: Record<"key", string>): string =>
