60a66af761a47102d83ac114b48d710f46faf1cf
Craig Cooper <c@c9r.dev>
2021-10-22 11:13:11 +0100

    fix: Readd Breakpoint for Button Group (#37)

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,4 +1,6 @@
 import { createTheme } from "@material-ui/core/styles/";
+import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
+
 import type {} from "@material-ui/lab/themeAugmentation"; // Required to add autocomplete to Theme until v5
 
 export const white = "#FFFFFF";
@@ -11,6 +13,8 @@ export const black = "#000000";
 export const mainGreen = "#02FFAD";
 export const purple = "#AE79FF";
 export const blue = "#00DBFF";
+
+const breakpoints = createBreakpoints({});
 
 export const theme = createTheme({
   palette: {
@@ -173,7 +177,13 @@ export const theme = createTheme({
         background: lightestGrey,
       },
       groupedVertical: {
-        padding: "8px 0px 8px 0px",
+        // padding: "8px 0px 8px 0px",
+        paddingLeft: "8px",
+        paddingRight: "8px",
+        [breakpoints.down("md")]: {
+          paddingLeft: "0px",
+          paddingRight: "0px",
+        },
       },
       groupedOutlinedVertical: {
         border: "none",
