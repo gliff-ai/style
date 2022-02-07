a18efb9cc984f3ec32b98e4ba201f267de68dd11
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-02-07 14:20:10 +0000

    feat: style tooltip

--- src/BaseHtmlTooltip.tsx
+++ src/BaseHtmlTooltip.tsx
@@ -4,11 +4,11 @@ import { veryLightBlue, white, darkGrey, theme } from "./theme";
 
 export const HtmlTooltip = withStyles(() => ({
   tooltip: {
-    backgroundColor: white,
+    backgroundColor: `${white} !important`,
     fontSize: theme.typography.pxToRem(12),
     border: "1px solid",
     borderColor: veryLightBlue,
-    color: darkGrey,
+    color: `${darkGrey} !important`,
     maxWidth: "100%",
     padding: "6px",
     width: "auto",
