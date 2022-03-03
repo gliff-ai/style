6d5d887871d7b4a3e33c366ab65eff8c89ae3317
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-03-03 13:53:38 +0000

    feat: move some components to STYLE

---
+++ src/Misc.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from "react";
+
+import {
+  Typography,
+  Card,
+  Paper,
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  TextField,
+  MenuItem,
+  Checkbox,
+  InputBase,
+} from "@mui/material";
+
+export {
+  RadioGroup, // done
+  Typography, // done
+  Card,
+  MenuItem, // done
+  FormControl, // done
+  FormControlLabel, // done
+  Radio, // done
+  Checkbox, // done
+  InputBase,
+};
