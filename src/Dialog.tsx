e09c055364fa4a9d5dee563c04bd0aaca6265d17
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-04-14 17:16:22 +0100

    feat: use card component

--- src/Dialog.tsx
+++ src/Dialog.tsx
@@ -1,18 +1,11 @@
 import { ReactElement, useState, cloneElement } from "react";
 
-import {
-  Card,
-  Paper,
-  Dialog as MaterialDialog,
-  Typography,
-  IconButton as MaterialIconButton,
-} from "@mui/material";
-import SVG from "react-inlinesvg";
-import { theme } from "./theme";
-import { icons } from "./icons";
+import { Dialog as MaterialDialog } from "@mui/material";
+
+import { Card } from "./Card";
 
 interface Props {
-  children?: JSX.Element[] | JSX.Element | null;
+  children?: ReactElement | null;
   TriggerButton: JSX.Element;
   title: string;
 }
@@ -33,41 +26,13 @@ export function Dialog({
   };
 
   const dialogContent = (
-    <Card sx={{ borderRadius: "9px" }}>
-      <Paper
-        elevation={0}
-        variant="outlined"
-        square
-        sx={{
-          p: "10px",
-          backgroundColor: theme.palette.primary.main,
-          position: "relative",
-        }}
-      >
-        <Typography
-          sx={{
-            fontSize: "21px",
-            width: "240px",
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-          }}
-        >
-          {title}
-        </Typography>
-        <MaterialIconButton
-          onClick={handleClose}
-          size="small"
-          sx={{ position: "absolute", top: "7px", right: "5px" }}
-        >
-          <SVG src={icons.removeLabel} width="15px" />
-        </MaterialIconButton>
-      </Paper>
-
-      <Paper elevation={0} sx={{ p: "20px" }}>
-        {children}
-      </Paper>
-    </Card>
+    <Card
+      title={title}
+      handleClose={handleClose}
+      // eslint-disable-next-line react/no-children-prop
+      children={children}
+      closeButton
+    />
   );
   return (
     <>
