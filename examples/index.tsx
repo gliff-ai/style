3a00a9f90fc4cdd1ca236870112289db5ff96d43
Chris <chris_pens@hotmail.co.uk>
2022-05-16 23:22:48 +0100

    feat: pass columns and rows as prop

--- examples/index.tsx
+++ examples/index.tsx
@@ -192,6 +192,42 @@ const ExamplePopper = (): ReactElement => {
   );
 };
 
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "age",
+    headerName: "Age",
+    type: "number",
+    width: 110,
+    editable: true,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
 const app = (
   <StylesProvider>
     <StyledEngineProvider injectFirst>
@@ -204,7 +240,7 @@ const app = (
           <BaseTextButton text="Hello World" />
           <br></br>
           <br></br>
-          <DataGridDemo />
+          <DataGridDemo columns={columns} rows={rows} />
           <br></br>
           <br></br>
           <Table />
