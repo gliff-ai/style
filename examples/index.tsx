70e88e382b7e0d7597a5135f0324024a5c683495
Philip Jackson <philiptgjackson@gmail.com>
2022-05-24 09:26:13 +0100

    fix: fill popover TriggerButton when open

--- examples/index.tsx
+++ examples/index.tsx
@@ -372,6 +372,7 @@ const app = (
 
           <Popover
             title="Example Popover"
+            anchorOrigin={{horizontal: "right", vertical: "top"}}
             TriggerButton={
               <IconButton
                 tooltip={{
