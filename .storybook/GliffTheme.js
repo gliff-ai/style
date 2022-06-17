7baea8d4057988b2cbec74502f2d7453e8c87a27
Chris <chris_pens@hotmail.co.uk>
2022-06-17 05:19:22 +0100

    fix: logo not showing in production build

--- .storybook/GliffTheme.js
+++ .storybook/GliffTheme.js
@@ -1,9 +1,10 @@
 import { create } from '@storybook/theming';
 
+
 export default create({
   base: 'light',
   brandTitle: 'gliff.ai',
   brandUrl: 'https://gliff.ai',
-  brandImage: `src/assets/gliff-master-black.svg `,
+  brandImage: '/src/assets/gliff-master-black.svg',
   brandTarget: '_blank',
 });
