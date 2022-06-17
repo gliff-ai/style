424f81748067cec625c6f09000ea6dfd64fe3356
Chris <chris_pens@hotmail.co.uk>
2022-06-17 08:10:34 +0100

    fix: rename logo for it to  work?

--- .storybook/GliffTheme.js
+++ .storybook/GliffTheme.js
@@ -1,10 +1,12 @@
 import { create } from '@storybook/theming';
 
+// const imgSrc = (src, type = "svg") =>
+//   new URL(`/src/assets/${src}.${type}`, import.meta.url).href;
 
 export default create({
   base: 'light',
   brandTitle: 'gliff.ai',
   brandUrl: 'https://gliff.ai',
-  brandImage: '/src/assets/gliff-master-black.svg',
+  brandImage: "src/assets/gliff.svg",
   brandTarget: '_blank',
 });
