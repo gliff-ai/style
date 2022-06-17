2dcbe4213ec225c1cf62ec72a06ddce4bc6b9be6
Chris <chris_pens@hotmail.co.uk>
2022-06-17 08:39:23 +0100

    fix: path to logo?

--- .storybook/GliffTheme.js
+++ .storybook/GliffTheme.js
@@ -1,12 +1,12 @@
 import { create } from '@storybook/theming';
+import logo from "../src/assets/gliff.svg";
 
-// const imgSrc = (src, type = "svg") =>
-//   new URL(`/src/assets/${src}.${type}`, import.meta.url).href;
+
 
 export default create({
   base: 'light',
   brandTitle: 'gliff.ai',
   brandUrl: 'https://gliff.ai',
-  brandImage: "src/assets/gliff.svg",
+  brandImage: logo,
   brandTarget: '_blank',
 });
